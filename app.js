ebe19f868cdfdd76d3513364e1d5137f8734685b
Matt Rantala <MapRantala@Gmail.com>
2020-02-06 21:52:34 -0600

    Added files and changes required for deployment

--- app.js
+++ app.js
@@ -17,7 +17,8 @@ app.use(compression()); //Compress all routes
 
 //Set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb+srv://Peggie1948:b00ksIl0v3@cluster0-ryw1n.gcp.mongodb.net/local_library?retryWrites=true&w=majority';
+var dev_db_url = 'mongodb+srv://Peggie1948:b00ksIl0v3@cluster0-ryw1n.gcp.mongodb.net/local_library?retryWrites=true&w=majority';
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true });
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
